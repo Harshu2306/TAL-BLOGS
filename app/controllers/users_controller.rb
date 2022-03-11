9590e967a116f5e1b0f682154d365dbbe0a814fd
Harshu2306 <harshpatelvi.dev@gmail.com>
2022-03-11 19:54:09 +0530

    [Change reppo]

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,58 @@
+class UsersController < ApplicationController
+
+	def show
+		@user=User.find(params[:id])
+		@articles=@user.articles
+	end
+
+	def index
+
+		@users = User.all
+
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def edit
+		@user=User.find(params[:id])
+	end
+
+	def update
+
+		@user=User.find(params[:id])
+		if @user.update(user_params)
+			flash[:notice]="Your Account Information is updated successfully"
+			redirect_to articles_path
+
+		else
+			render 'edit'
+
+		end
+		
+	end
+
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:notice]="Welcome to TAL-BLOGS #{@user.username} , You have signed up successfully!!"
+			redirect_to articles_path
+
+		else
+			render 'new'
+		end
+
+	end
+
+private 
+
+def user_params
+
+	params.require(:user).permit(:username,:email,:password)
+
+end
+
+end
+
